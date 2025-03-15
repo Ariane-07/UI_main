02347583cf0b158670f81e9d6ff040b9f4580afe
JARP <andersonandy046@gmail.com>
2025-03-15 14:44:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyPet</title>
+    <link rel="icon" type="image/png" href="assets/imgs/Logo.png">
     <link rel="stylesheet" href="assets/css/Style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
