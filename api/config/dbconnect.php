51f2759fae919ea43a17056cc3df8172c128eaa5
JARP <andersonandy046@gmail.com>
2025-03-30 16:57:55 +0800

    update connection

--- api/config/dbconnect.php
+++ api/config/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
 define("db_host", "localhost");
-define("db_user", "root");
-define("db_pass", "");
-define("db_name", "mypet");
+define("db_user", "u491760130_mypet");
+define("db_pass", "U491760130_mypet");
+define("db_name", "u491760130_mypet");
 
 class db_connect
 {
